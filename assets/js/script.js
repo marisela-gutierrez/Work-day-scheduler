52199b9c17404940b7854751da6f07dcef53762b
marigut <mari.gutierrez96@yahoo.com>
2021-04-25 19:15:43 -0500

    localStorage and timeCheck

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,55 +1,55 @@
 var events = {};
 
-var createEvent = [];
-
-
+var currentDayEl = moment();
+$("#currentDay").text(currentDayEl.format("dddd MMMM YYYY"));
 
 
 var loadEvents = function() {
   events = JSON.parse(localStorage.getItem("events"));
 
-  // if nothing in localStorage, create a new object to track all event status arrays
+  // if nothing in localStorage, create a new object to track all task status arrays
   if (!events) {
-    events = {
-      toDo: [],
-      inProgress: [],
-      inReview: [],
-      done: []
-    };
+    events = ["", "", "", "", "", "", "", "", ""];
   }
 
-// modal was triggered
-$("#eventPlan").on("click", "textarea", function() {
-  // get current text
-  var eventAgenda = $(this)
-    .text()
-    .trim();
-
-  // create new input element
-  var eventInput = $("#eventDescription")
-    .attr("type", "text")
-    .addClass("form-control")
-    .val(eventAgenda);
-  $(this).replaceWith(eventInput);
-
-  var saveEvents = function() {
-    localStorage.setItem("events", JSON.stringify(events));
-  };
-
-  // save button in modal was clicked
-$("#eventPlan .saveBtn").click(function() {
-  // get form values
-  var eventText = $("#eventDescription").val();
-
-  if (eventText) {
-    createEvent();
-
-    // save events in array
-    events.toDo.push({
-      text: eventText
-    });
+  for (let i =0; i < events.length; i++) {
+    $("#" + i + "H").val(events[i]);
   }
+
+};
+ 
+
+var saveEvent = function() {
+  localStorage.setItem("events", JSON.stringify(events));
+};
+
+// save button was clicked
+$(".saveBtn").click(function() {
+  // get text values
+  var id = this.id;
+  var eventTask = $("#" + id + "H").val();
+
+  if (eventTask) {
+    events[parseInt(id)] = eventTask;
+  }
+  saveEvent();
 });
 
-// load events for the first time
+var checkTime = function() {
+  var presentHour = parseInt(currentDayEl.format("H"));
+
+  for (i = 9; i <= 17; i++) {
+    if (i == presentHour) {
+      $("#" + i + "H").addClass("present");
+    } else if (i < presentHour) {
+      $("#" + i + "H").addClass("past");
+    } else if (i > presentHour) {
+      $("#" + i + "H").addClass("future");
+    }
+
+  }
+
+};
+
 loadEvents();
+checkTime();
